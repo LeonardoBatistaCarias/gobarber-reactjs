67e149146f4e358b11083dcdab3bb4b7cda05a72
LeonardoBatistaCarias <leonardo1carias@gmail.com>
2020-01-25 10:55:06 -0300

    Rotas privadas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
@@ -15,6 +16,8 @@ export default function Routes() {
 
       <Route path="/dasboard" exact component={Dashboard} />
       <Route path="/profile" component={Profile} />
+
+      <Route path="/" component={() => <h1>404</h1>} />
     </Switch>
   );
 }
